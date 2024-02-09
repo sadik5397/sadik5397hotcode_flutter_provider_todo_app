4af099e8f5f65d7ff8d6e71269e24dbab90f7d7d
sadik5397@gmail.com </student/>
2024-02-09 11:17:59 +0600

    UI Complete

--- lib/main.dart
+++ lib/main.dart
@@ -20,10 +20,33 @@ class _TodoPageState extends State<TodoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("ToDo"),),
-        body: Container(
-            child: 
-        ),
+      appBar: AppBar(
+        title: Text("ToDo"),
+        actions: [Text("Show Completed"), Switch(value: false, onChanged: (value) {})],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(24),
+            child: TextField(
+              autofocus: true,
+              controller: TextEditingController(),
+              onSubmitted: (value) {},
+              decoration: InputDecoration(suffixIcon: IconButton(onPressed: () {}, icon: Icon(Icons.adaptive.arrow_forward), iconSize: 18)),
+            ),
+          ),
+          Expanded(
+              child: ListView.builder(
+                  itemCount: 20,
+                  itemBuilder: ((context, index) => ListTile(
+                        onTap: () {},
+                        onLongPress: () {},
+                        title: Text("Hello $index"),
+                        subtitle: Text("Created on"),
+                        leading: Checkbox(value: false, onChanged: (value) {}),
+                      ))))
+        ],
+      ),
     );
   }
 }
