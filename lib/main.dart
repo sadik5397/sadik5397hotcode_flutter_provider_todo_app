4523eb0d488c739a7ff16169b22dc06cd0e7b71e
sadik5397@gmail.com </student/>
2024-02-09 13:11:56 +0600

    Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'provider.dart';
 
 void main() => runApp(const MyApp());
 
@@ -6,47 +11,55 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) => MaterialApp(debugShowCheckedModeBanner: false, title: 'ToDos', theme: ThemeData(useMaterial3: true), home: const TodoPage());
+  Widget build(BuildContext context) => MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => ToDoProvider())],
+      child: MaterialApp(themeMode: ThemeMode.dark, debugShowCheckedModeBanner: false, title: 'ToDos', theme: ThemeData(useMaterial3: true), home: const TodoPage()));
 }
 
-class TodoPage extends StatefulWidget {
+class TodoPage extends StatelessWidget {
   const TodoPage({Key? key}) : super(key: key);
 
   @override
-  State<TodoPage> createState() => _TodoPageState();
-}
-
-class _TodoPageState extends State<TodoPage> {
-  @override
   Widget build(BuildContext context) {
+    Provider.of<ToDoProvider>(context, listen: false).readAllTasks();
     return Scaffold(
-      appBar: AppBar(
-        title: Text("ToDo"),
-        actions: [Text("Show Completed"), Switch(value: false, onChanged: (value) {})],
-      ),
-      body: Column(
-        children: [
+        floatingActionButton: Consumer<ToDoProvider>(builder: (context, toDoProvider, child) => FloatingActionButton(child: const Icon(Icons.refresh), onPressed: () => toDoProvider.readAllTasks())),
+        appBar: AppBar(title: const Text("ToDo"), actions: [
+          const Text("Show Completed  "),
+          Consumer<ToDoProvider>(builder: (context, toDoProvider, child) => Switch(value: toDoProvider.showCompletedStatus, onChanged: (value) => toDoProvider.toggleShowCompleted()))
+        ]),
+        body: Column(children: [
           Padding(
-            padding: const EdgeInsets.all(24),
-            child: TextField(
-              autofocus: true,
-              controller: TextEditingController(),
-              onSubmitted: (value) {},
-              decoration: InputDecoration(suffixIcon: IconButton(onPressed: () {}, icon: Icon(Icons.adaptive.arrow_forward), iconSize: 18)),
-            ),
+              padding: const EdgeInsets.all(24),
+              child: Consumer<ToDoProvider>(
+                  builder: (context, toDoProvider, child) => TextField(
+                      autofocus: true,
+                      controller: toDoProvider.newTaskController,
+                      onSubmitted: (value) => toDoProvider.createNewTask(),
+                      decoration: InputDecoration(hintText: "Add New Task", suffixIcon: IconButton(onPressed: () => toDoProvider.createNewTask(), icon: Icon(Icons.adaptive.arrow_forward), iconSize: 18))))),
+          Consumer<ToDoProvider>(
+            builder: (context, toDoProvider, child) => toDoProvider.allTasks.isEmpty
+                ? const SizedBox()
+                : Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24).copyWith(bottom: 6),
+                    child: Stack(alignment: Alignment.center, children: [
+                      LinearProgressIndicator(value: toDoProvider.completedTaskCount / toDoProvider.allTasks.length, minHeight: 24, borderRadius: BorderRadius.circular(4), color: Colors.purple.shade200),
+                      Text("${toDoProvider.completedTaskCount} Tasks Completed out of ${toDoProvider.allTasks.length}", style: const TextStyle(fontSize: 12))
+                    ]),
+                  ),
           ),
           Expanded(
-              child: ListView.builder(
-                  itemCount: 20,
-                  itemBuilder: ((context, index) => ListTile(
-                        onTap: () {},
-                        onLongPress: () {},
-                        title: Text("Hello $index"),
-                        subtitle: Text("Created on"),
-                        leading: Checkbox(value: false, onChanged: (value) {}),
-                      ))))
-        ],
-      ),
-    );
+              child: Consumer<ToDoProvider>(
+                  builder: (context, toDoProvider, child) => ListView.builder(
+                      itemCount: toDoProvider.allTasks.length,
+                      itemBuilder: ((context, index) => Visibility(
+                          visible: toDoProvider.showCompletedStatus || !jsonDecode(toDoProvider.allTasks[index])["status"],
+                          child: ListTile(
+                              onTap: () => toDoProvider.toggleTask(index),
+                              onLongPress: () => toDoProvider.deleteTask(index),
+                              title: Text(jsonDecode(toDoProvider.allTasks[index])["title"]),
+                              subtitle: Text(DateTime.fromMillisecondsSinceEpoch(jsonDecode(toDoProvider.allTasks[index])["created_on"]).toString()),
+                              leading: Checkbox(value: jsonDecode(toDoProvider.allTasks[index])["status"], onChanged: (value) => toDoProvider.toggleTask(index))))))))
+        ]));
   }
 }
---
+++ lib/provider.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ToDoProvider with ChangeNotifier {
+  void refresh() {
+    getCompletedTaskCount();
+    notifyListeners();
+  }
+
+  List<String> allTasks = [];
+  TextEditingController newTaskController = TextEditingController();
+  bool showCompletedStatus = false;
+  int completedTaskCount = 0;
+  readAllTasks() async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    allTasks = pref.getStringList("tasks") ?? [];
+    refresh();
+  }
+
+  toggleTask(int index) async {
+    Map selectedTask = jsonDecode(allTasks[index]);
+    Map modifiedTask = {"created_on": selectedTask["created_on"], "title": selectedTask["title"], "status": !(selectedTask["status"])};
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    allTasks = pref.getStringList("tasks") ?? [];
+    allTasks[index] = jsonEncode(modifiedTask);
+    pref.setStringList("tasks", allTasks);
+    refresh();
+  }
+
+  deleteTask(int index) async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    allTasks = pref.getStringList("tasks") ?? [];
+    allTasks.removeAt(index);
+    pref.setStringList("tasks", allTasks);
+    refresh();
+  }
+
+  createNewTask() async {
+    if (newTaskController.text.isNotEmpty) {
+      Map newTask = {"created_on": DateTime.now().millisecondsSinceEpoch, "title": newTaskController.text, "status": false};
+      final SharedPreferences pref = await SharedPreferences.getInstance();
+      allTasks = pref.getStringList("tasks") ?? [];
+      allTasks.add(jsonEncode(newTask));
+      pref.setStringList("tasks", allTasks);
+      newTaskController.clear();
+      refresh();
+    }
+  }
+
+  toggleShowCompleted() async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    showCompletedStatus = pref.getBool("showCompleted") ?? false;
+    showCompletedStatus = !showCompletedStatus;
+    pref.setBool("showCompleted", showCompletedStatus);
+    refresh();
+  }
+
+  getCompletedTaskCount() {
+    completedTaskCount = 0;
+    for (int i = 0; i < allTasks.length; i++) {
+      Map selected = jsonDecode(allTasks[i]);
+      selected["status"] == true ? completedTaskCount++ : null;
+    }
+  }
+}
